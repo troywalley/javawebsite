40d95734433003580a33d9568a15e862b6386fa3
Troy Walley <walley.troy@yahoo.com>
2017-04-16 17:44:35 -0400

    try website commit

--- scripts.js
+++ scripts.js
@@ -1,17 +1,22 @@
 var modal=document.getElementsByClassName("modalcontainer")[0];
+var modalx=document.getElementsByClassName("modal")[0];
 var overlays=document.getElementById("overlays");
+var submit=document.getElementsByClassName("submit")[0];
+var xout=document.getElementsByClassName("xout")[0];
 
-document.addEventListener("click", ShowModal);
 window.setTimeout(ShowModal,3000)
 function ShowModal(){
 	modal.style.display="block";
 	overlays.style.display="block";
-	document.addEventListener("keydown",CloseWindow)	
+	document.addEventListener("keydown",CloseWindow);	
 }
 function CloseWindow(event){
+	
 	if(event.keyCode===27){
-		modal.style.display="none"
-		overlays.style.display="none";
+		CloseModal();
+	} else if(event.keyCode===13){
+		alert("Thanks! You have been signed up for our newsletter.")
+		CloseModal();
 	}
 }
 function Carousel(){
@@ -43,4 +48,27 @@ function Pic4(){
 function Pic5(){
 	carousel.style.backgroundImage="url('construction5.jpg')"
 	window.setTimeout(Carousel,3000)
+}
+
+
+
+function CloseModal(){
+	modal.style.display="none";
+	overlays.style.display="none";
+}
+submit.addEventListener("click", function(){
+	
+	alert("Thanks! You have been signed up for our newsletter.")
+	CloseModal();
+})
+xout.addEventListener("click",CloseModal);
+var hambox=document.getElementsByClassName("hambox")[0];
+var hambutton=document.getElementById("hamburgerbutton");
+hambutton.addEventListener("click",ShowHamBox);
+function ShowHamBox(){
+	if(hambox.style.display==="none"){
+		hambox.style.display="block"
+	}else{
+		hambox.style.display="none"
+	}
 }
